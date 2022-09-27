724e53c940c22e2c6d354f8bbaa3d90853d9ff9e
Victor Njenga <84647506+victornjenga@users.noreply.github.com>
2022-09-27 14:24:15 +0300

    change1

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const TokenRoute = require("./routes/token");
-const PORT=5000
-app.listen(PORT , () => {
+
+app.listen(5000, (req, res) => {
   console.log("server is running");
 });
-
+app.get("/",(req,res)=>{
+  res.send("Server is running")
+})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
