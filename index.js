8eaf01e53c12fe49dc5366dea11b367a2378f50e
Victor Njenga <84647506+victornjenga@users.noreply.github.com>
2022-11-27 00:20:27 +0300

    error fixed

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const cors = require("cors");
 const axios = require("axios");
 const TokenRoute = require("./routes/token");
 
-app.listen(5000, () => {
-  console.log("server run nicely");
-});
-// app.use(cors);
+// app.listen(5000, () => {
+//   console.log("server run nicely");
+// });
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
